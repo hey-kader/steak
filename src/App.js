38b0bd547314b687f593c9bcd12c93593d877b1c
pablopenrose <local@kader.email>
2021-07-21 01:49:17 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <nav>
+              <ul>
+                  <Router>
+                      <Switch>
+                          <Route to="/menus">
+                              <li>menus</li>
+                          </Route>
+                      </Switch>
+                      <Switch>
+                          <Route to="/hours">
+                              <li>hours</li>
+                          </Route>
+                      </Switch>
+                      <Switch>
+                          <Route to="/more">
+                              <li>more</li>
+                          </Route>
+                      </Switch>
+                  </Router>
+              </ul>
+          </nav>
       </header>
     </div>
   );
