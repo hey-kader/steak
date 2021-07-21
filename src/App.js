7c1b8d4e45de27530fa4f4dd791636a68db1eaff
pablopenrose <local@kader.email>
2021-07-21 03:30:17 -0400

    navbar functionality

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 import './App.css'
+import React, {Component} from 'react'
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+import Menus from './comps/Menus'
+import Hours from './comps/Hours'
+import More from './comps/More'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <nav>
-              <ul>
-                  <Router>
-                      <Switch>
-                          <Route to="/menus">
-                              <li>menus</li>
-                          </Route>
-                      </Switch>
-                      <Switch>
-                          <Route to="/hours">
-                              <li>hours</li>
-                          </Route>
-                      </Switch>
-                      <Switch>
-                          <Route to="/more">
-                              <li>more</li>
-                          </Route>
-                      </Switch>
-                  </Router>
-              </ul>
-          </nav>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+              <nav>
+                  <ul>
+                      <Router>
+                          <Switch>
+                              <Route exact path="/" />
+                              <Route path="/menus" component={Menus} />
+                              <Route path="/hours" component={Hours} />
+                              <Route path="/more" component={More} />
+                          </Switch>
+
+                          <Link to="/menus">menus</Link>
+                          <Link to="/hours">hours</Link>
+                          <Link to="/more" >more</Link>
+                          <Link to="/" >home</Link>
+                      </Router>
+                  </ul>
+              </nav>
+          </header>
+        </div>
+      );
+    }
 }
 
 export default App;
