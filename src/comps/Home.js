9465c3a2bb16b7bca97cac7a4c8a18545a1c6417
pablopenrose <kaderarnold@icloud.com>
2021-07-21 13:55:02 -0700

    organized into a backend and frontend

--- src/comps/Home.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react"
-
-class Home extends Component {
-    render () {
-        return (<img className="logo" width="250px" src="/rittenhouse_grill_logo.png" alt="Rittenhouse Gr    ill Logo" />)
-    }
-}
-
-export default Home
--- src/comps/Menus.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react'
-
-class Menus extends Component {
-    render () {
-        return (<h1>menus</h1>)
-    }
-}
-
-export default Menus
