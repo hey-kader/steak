2515b70fecae170afaba07c5402b8f995c9a5f88
pablopenrose <kaderarnold@icloud.com>
2021-07-21 17:44:48 -0700

    current production run

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 var express = require ('express')
 const path = require ('path')
 const cors = require ('cors')
+const fs = require ('fs')
+const https = require ('https')
 
 const PORT = 80
 const IP = "209.188.7.188"
@@ -11,7 +13,11 @@ var app = express()
 app.use (express.static(path.join(__dirname, 'build')))
 app.set ('domain', 'rittenhouse.restaurant')
 app.use(cors())
+app.use(redirectUnmatched)
 
+function redirectUnmatched (req, res) {
+  res.redirect('https://rittenhouse.restaurant/')
+}
 
 app.get ('/' , function (req, res) {
   res.sendFile(path.join(build, 'index.html'))
@@ -19,7 +25,19 @@ app.get ('/' , function (req, res) {
 
 
 
-app.listen(PORT, IP, function () {
-  console.log('listening at '+IP+':'+PORT)
+//app.listen(PORT, IP, function () {
+//console.log('listening at '+IP+':'+PORT)
+//})
+
+const credentials = {
+  key: fs.readFileSync('rittenhouse.restaurant.key'),
+  cert: fs.readFileSync('rittenhouse_restaurant.crt')
+}
+
+const server = https.createServer (credentials, app)
+server.listen('443', IP, function () {
+console.log('running on https://rittenhouse.restaurant')
 })
+
+
 
